766db47429ec9872a97d7a16f80dba82a3f47c1e
mentol <mentol310@gmail.com>
2016-06-29 01:57:40 +0900

    It summarizes the header icon

--- components/Header.js
+++ components/Header.js
@@ -1,6 +1,34 @@
 import React from 'react'
 import AppBar from 'material-ui/AppBar'
-import HeaderIcon from './HeaderIcon'
+import IconButton from 'material-ui/IconButton';
+import IconMenu from 'material-ui/IconMenu';
+import MenuItem from 'material-ui/MenuItem';
+import MenuIcon from 'material-ui/svg-icons/navigation/menu';
+import FilterLink from '../containers/FilterLink'
+
+const HeaderIcon = () => (
+  <IconMenu
+    iconButtonElement={<IconButton><MenuIcon color={'white'} /></IconButton>}
+    anchorOrigin={{horizontal: 'left', vertical: 'top'}}
+    targetOrigin={{horizontal: 'left', vertical: 'top'}}
+  >
+    <MenuItem>
+      <FilterLink filter="SHOW_ALL">
+        All
+      </FilterLink>
+    </MenuItem>
+    <MenuItem>
+      <FilterLink filter="SHOW_ACTIVE">
+        Active
+      </FilterLink>
+    </MenuItem>
+    <MenuItem>
+      <FilterLink filter="SHOW_COMPLETED">
+        Completed
+      </FilterLink>
+    </MenuItem>
+  </IconMenu>
+);
 
 const Header = () => (
   <AppBar
